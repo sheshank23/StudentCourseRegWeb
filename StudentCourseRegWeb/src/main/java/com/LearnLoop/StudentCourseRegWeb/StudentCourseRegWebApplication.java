2c4ce964942cd740ae1cc0a62ec6c4e62a0c5094
sheshank23 <sheshank6633@gmail.com>
2025-05-15 22:10:13 +0530

    initial commit

---
+++ StudentCourseRegWeb/src/main/java/com/LearnLoop/StudentCourseRegWeb/StudentCourseRegWebApplication.java
@@ -0,0 +1,13 @@
+package com.LearnLoop.StudentCourseRegWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCourseRegWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentCourseRegWebApplication.class, args);
+	}
+
+}
---
+++ StudentCourseRegWeb/src/test/java/com/LearnLoop/StudentCourseRegWeb/StudentCourseRegWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LearnLoop.StudentCourseRegWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCourseRegWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
